1ccddce7a479be8d18b8d6673e03b4d84786d85f
Divya Gupta <117669677+Divsgupta12@users.noreply.github.com>
2023-01-17 11:20:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your code here
+let evaluatedText = document.getElementById('evaluatedText');
+        let result = document.getElementById('letterCount');
+        evaluatedText.addEventListener("input" , countword);
+        function countword(){
+            word = evaluatedText.value;
+            result.innerHTML = word.length;
+        }
